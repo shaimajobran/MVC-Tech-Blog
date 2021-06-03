df2f74d123e4f49605363f7b418d803f9241447f
shaimaJobran <shaimajobran222gmail.com>
2021-06-03 17:58:05 -0400

    connect to jawsdb

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
   
-const User = require('./user');
-const Post = require('./post');
-const Comment = require('./comment');
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
 
 // create associations
